16c1bb45d021b7fadef9b6b631efa6b06b721b62
Kássia <kassia_guterres@outlook.com>
2024-03-15 20:15:59 -0300

    project configuration

---
+++ Front/betIt20/src/compoments/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import GlobalStyle from "../assets/style/GlobalStyle"
+import Home from './home/Home';
+
+export default function App() {
+ 
+  return (
+    <>
+      <GlobalStyle/>
+      <Home></Home>
+    </>
+
+  )
+}
+
+
